c290a2583522d56e73bf30a32ae6e260d13e8a3f
OHE Shogo <shogo.ohe.47@gmail.com>
2022-07-25 01:02:59 +0900

    add functions-v4-javascript/QueueStorageOutput1

--- functions-v4-javascript/QueueStorageOutput1/index.js
+++ functions-v4-javascript/QueueStorageOutput1/index.js
@@ -1,11 +1,26 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    const crypto = require('crypto');
+    const timeStamp = new Date().toISOString();
 
-    const name = (req.query.name || (req.body && req.body.name));
+    //const name = (req.query.name || (req.body && req.body.name));
+    const name = 'azure';
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+
+    var message = 'Message created at: ' + timeStamp +' '+ crypto.randomUUID();
+    // how many message create?
+    const numOfMessage = Math.floor(Math.random() * 10);
+    
+    context.bindings.myQueueItem = []
+    for (let i = 0; i <= numOfMessage; i++) {
+        context.bindings.myQueueItem.push(i +"of"+ numOfMessage +' '+ message);
+    }
+    //context.bindings.myQueueItem = ["message 1","message 2"];
+    context.done();
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
